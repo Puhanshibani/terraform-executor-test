61cf98ea25ed134d142bedf5332f71b40d247516
Puhanshibani <43839320+Puhanshibani@users.noreply.github.com>
2022-06-07 17:28:51 +0530

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,5 +3,5 @@ import subprocess
 p = subprocess.Popen("ls -lh", stdout=subprocess.PIPE, shell=True)
 print(p.communicate())
 
-subprocess.run(["./entrypoint.sh"])
+subprocess.run(["sh entrypoint.sh"])
 print("Hello Python")
