dee4a16d21326279328d58daaf11e9d72c7a50f7
Puhanshibani <43839320+Puhanshibani@users.noreply.github.com>
2022-06-08 15:23:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ RUN curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform
   unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /bin && \
   rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 ls -ltr
+terraform --version
 python3.9 --version
 python3.9 entrypoint.py
