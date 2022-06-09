6df0a1127e58a27b011666885b380cd54fb985af
Puhanshibani <43839320+Puhanshibani@users.noreply.github.com>
2022-06-09 22:55:56 +0530

    Update _terraform.tf

--- _terraform.tf
+++ _terraform.tf
@@ -1,9 +1,9 @@
- terraform.tf : Location of the terraform state file . (s3)key is unique to each code pipeline
+#  terraform.tf : Location of the terraform state file . (s3)key is unique to each code pipeline
 
-terraform {
-  backend "s3" {
-    bucket         = "terraform-executer-tf-remote-state"           # replace with remote_state_bucket name
-    region         = "eu-west-1"                                  # replace with deployment region
-    key            = "pod_name/terraform.tfstate" # replace with name of the repo which will define resources
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-executer-tf-remote-state"           # replace with remote_state_bucket name
+#     region         = "eu-west-1"                                  # replace with deployment region
+#     key            = "pod_name/terraform.tfstate" # replace with name of the repo which will define resources
+#   }
+# }
