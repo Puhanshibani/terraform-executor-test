8157a99732bfd24f2c27999bfd18613a125ad337
Puhanshibani <43839320+Puhanshibani@users.noreply.github.com>
2022-06-09 22:26:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-# provider "aws" {
-#   region     = var.region
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_key
-# }
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 
 
 
